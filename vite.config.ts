9e98cdd450b66926cab80ce9573743345a832388
Bati <no-reply@batijs.dev>
2025-01-07 20:15:54 +0900

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [vike({}), react({})],
+
+  resolve: {
+    alias: {
+      "@": new URL("./", import.meta.url).pathname,
+    },
+  },
+});
