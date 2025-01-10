2ed7e4d9bad67d6d761b6038e477c8058238fd1a
Dian Nandiwardhana <o_dian.nandiwardhana@nds-osk.co.jp>
2025-01-11 00:36:33 +0900

    move site pages to site route group

---
+++ pages/(site)/users/+guard.ts
@@ -0,0 +1,18 @@
+import { render } from "vike/abort";
+import { PageContext } from "vike/types";
+
+import Role from "@/server/constants/role";
+
+export const guard = (pageContext: PageContext) => {
+  const { user } = pageContext;
+
+  if (!user) {
+    // Render the login page while preserving the URL.
+    throw render("/auth/login");
+  } else {
+    if (!user.role.includes(Role.Admin)) {
+      // Render the error page and show message to the user
+      throw render(403, "Only admins are allowed to access this page.");
+    }
+  }
+};
---
+++ pages/(site)/users/index/Users.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { withFallback } from "vike-react-query";
+import { Config } from "vike-react/Config";
+import { Head } from "vike-react/Head";
+import { User } from "../types";
+import API from "@/config/apiClient";
+
+const Users = withFallback(
+  () => {
+    const { data } = useSuspenseQuery({
+      queryKey: ["users"],
+      queryFn: async () => {
+        return await API.get("/api/user/all");
+      },
+      staleTime: 60 * 1000,
+    });
+
+    const users = data as unknown;
+
+    return (
+      <>
+        <Config title={`${(users as User[]).length} users`} />
+        <Head>
+          <meta name="description" content={`List of ${(users as User[]).length} users.`} />
+        </Head>
+        <div>
+          {(users as User[]).map(({ email }: User) => (
+            <ul key={email}>
+              <li className="capitalize">Email: {email}</li>
+            </ul>
+          ))}
+        </div>
+      </>
+    );
+  },
+  () => <div>Loading users...</div>,
+  // The props `retry` and `error` are provided by vike-react-query
+  ({ retry, error }) => (
+    <div>
+      <div>Failed to load Users: {error.message}</div>
+      <button onClick={() => retry()}>Retry</button>
+    </div>
+  ),
+);
+
+export { Users };
