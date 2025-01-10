c77b64b51d0179304432a67eb2214c799416df2d
Dian Nandiwardhana <o_dian.nandiwardhana@nds-osk.co.jp>
2025-01-11 00:40:04 +0900

    modify error page

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { usePageContext } from "vike-react/usePageContext";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
+
 export default function Page() {
-  const { is404 } = usePageContext();
+  const { is404, abortReason } = usePageContext();
   if (is404) {
     return (
-      <>
-        <h1>404 Page Not Found</h1>
-        <p>This page could not be found.</p>
-      </>
+      <Alert variant="destructive">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>404 Page Not Found</AlertTitle>
+        <AlertDescription>This page could not be found.</AlertDescription>
+      </Alert>
     );
   }
-  return (
-    <>
-      <h1>500 Internal Server Error</h1>
-      <p>Something went wrong.</p>
-    </>
+  return abortReason ? (
+    <Alert variant="destructive">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>{abortReason as string}</AlertTitle>
+      <AlertDescription>{abortReason as string}</AlertDescription>
+    </Alert>
+  ) : (
+    <Alert variant="destructive">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>500 Internal Server Error</AlertTitle>
+      <AlertDescription>Something went wrong.</AlertDescription>
+    </Alert>
   );
 }
