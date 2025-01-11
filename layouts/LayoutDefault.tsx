495414313795854ec0818c1b1939bf6de27c3ce8
Dian Nandiwardhana <o_dian.nandiwardhana@nds-osk.co.jp>
2025-01-11 10:26:25 +0900

    modify default layout

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -3,5 +3,5 @@ import React from "react";
 import "./tailwind.css";
 
 export default function LayoutDefault({ children }: { children: React.ReactNode }) {
-  return <div id="App">{children}</div>;
+  return <>{children}</>;
 }
