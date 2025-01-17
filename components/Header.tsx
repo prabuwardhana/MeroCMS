9512e96f7d4eac7cc7585b38814d8d7a1b9ec3c9
prabuwardhana <prabuwardhana03@yahoo.com>
2025-01-17 21:26:14 +0900

    add dropdown menu for logging out from admin dashboard

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,19 @@
 import React, { Dispatch, SetStateAction } from "react";
 
-import { Bell, ChevronsLeft } from "lucide-react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { navigate } from "vike/client/router";
+
+import { Bell, ChevronsLeft, LogOut } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import API from "@/config/apiClient";
 
 interface HeaderProps {
   collapsed: boolean;
@@ -10,6 +21,16 @@ interface HeaderProps {
 }
 
 const Header = ({ collapsed, setCollapsed }: HeaderProps) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async () => API.get("/api/auth/logout"),
+    onSettled: () => {
+      queryClient.clear();
+      navigate("/auth/login");
+    },
+  });
+
   return (
     <header className="transition-color relative z-10 flex h-[60px] items-center justify-between bg-background px-4 shadow-md">
       <div className="flex items-center gap-x-3">
@@ -21,12 +42,23 @@ const Header = ({ collapsed, setCollapsed }: HeaderProps) => {
         <button className="btn-ghost size-10">
           <Bell size={18} />
         </button>
-        <button className="size-10 overflow-hidden rounded-full">
-          <Avatar>
-            <AvatarImage src="/assets/logo.svg" />
-            <AvatarFallback className="text-black">BT</AvatarFallback>
-          </Avatar>
-        </button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="size-10 overflow-hidden rounded-full">
+              <Avatar>
+                <AvatarImage src="/assets/logo.svg" />
+                <AvatarFallback className="text-black">BT</AvatarFallback>
+              </Avatar>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => mutation.mutate()}>
+              <LogOut />
+              <span>Log out</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
