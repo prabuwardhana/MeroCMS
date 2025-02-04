7cdf91aa5f15513f6383773f4a19ad78e566ebad
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-04 17:06:02 +0900

    modify posts and categories table column definition!

--- pages/admin/categories/categoriesColumnDef.tsx
+++ pages/admin/categories/categoriesColumnDef.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ColumnDef } from "@tanstack/react-table";
 import { CategoryType } from "@/lib/types";
 import { DataTableColumnHeader, DataTableRowActions } from "@/components/DataTable";
+import { Checkbox } from "@/components/ui/checkbox";
 
 interface CategoriesColumnsProps {
   onEdit: (category: CategoryType) => void;
@@ -9,6 +10,27 @@ interface CategoriesColumnsProps {
 }
 
 export const getCategoriesColumns = ({ onEdit, onDelete }: CategoriesColumnsProps): ColumnDef<CategoryType>[] => [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: "name",
     header: ({ column }) => <DataTableColumnHeader column={column} title="Category Name" />,
--- pages/admin/posts/postsColumnDef.tsx
+++ pages/admin/posts/postsColumnDef.tsx
@@ -3,6 +3,8 @@ import { ColumnDef } from "@tanstack/react-table";
 import { CategoryType, PostType } from "@/lib/types";
 import { DataTableColumnHeader, DataTableRowActions } from "@/components/DataTable";
 import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
+import { BookPlus, PencilLine } from "lucide-react";
 
 interface CategoriesColumnsProps {
   onEdit: (category: PostType) => void;
@@ -10,6 +12,27 @@ interface CategoriesColumnsProps {
 }
 
 export const getPostsColumns = ({ onEdit, onDelete }: CategoriesColumnsProps): ColumnDef<PostType>[] => [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: "title",
     header: ({ column }) => <DataTableColumnHeader column={column} title="Title" />,
@@ -21,6 +44,7 @@ export const getPostsColumns = ({ onEdit, onDelete }: CategoriesColumnsProps): C
     cell: (info) => info.getValue(),
   },
   {
+    id: "categories",
     accessorFn: (row) => row.categories.map((Item) => (Item as unknown as CategoryType).name).join(", "),
     header: "Categories",
     cell: (info) => {
@@ -35,16 +59,33 @@ export const getPostsColumns = ({ onEdit, onDelete }: CategoriesColumnsProps): C
         </div>
       );
     },
+    filterFn: (row, id, value) => {
+      const rowValue = (row.getValue(id) as string).split(", ");
+      const isIntersect = value.filter((item: string) => rowValue.includes(item)).length > 0;
+      return isIntersect;
+    },
   },
   {
-    accessorKey: "published",
+    id: "published",
+    accessorFn: (d) => d.published.toString(),
     header: ({ column }) => <DataTableColumnHeader column={column} title="Status" />,
     cell: (info) =>
-      info.getValue() === true ? (
-        <Badge className="bg-green-500">published</Badge>
+      info.getValue() === "true" ? (
+        <Badge className="bg-green-500 py-1 hover:bg-green-400">
+          <BookPlus size={16} className="mr-2" />
+          published
+        </Badge>
       ) : (
-        <Badge variant="destructive">draft</Badge>
+        <Badge variant="destructive" className="py-1">
+          <PencilLine size={16} className="mr-2" />
+          draft
+        </Badge>
       ),
+    filterFn: (row, id, value) => {
+      const rowValue = (row.getValue(id) as string).split(", ");
+      const isIntersect = value.filter((item: string) => rowValue.includes(item)).length > 0;
+      return isIntersect;
+    },
   },
   {
     id: "actions",
