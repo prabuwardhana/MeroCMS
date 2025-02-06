611bd35979e83df438ab36314c88528e3b7f7f4f
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-06 20:35:31 +0900

    remove unused store file

--- store/store.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import { createFileSlice } from "./fileSlice";
-import { Store } from "./types";
-
-export const useAppStore = create<Store>()((...a) => ({
-  ...createFileSlice(...a),
-}));
