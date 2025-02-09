3b4600e4e75bd2a2afb3c306e7e77f34fed3e2e0
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-09 23:03:38 +0900

    add fallback component to create or edit nav menu page

--- pages/admin/menu/CreateOrEditNavMenu.tsx
+++ pages/admin/menu/CreateOrEditNavMenu.tsx
@@ -1,24 +1,38 @@
 import React from "react";
 import { withFallback } from "vike-react-query";
+import { NestableItemsProvider } from "@/providers/nestableItemsProvider";
+import { Button } from "@/components/ui/button";
 import AddItemForm from "./AddItemForm";
 import MenuEditor from "./MenuEditor";
-import { NestableItemsProvider } from "@/providers/nestableItemsProvider";
+import { RotateCcw } from "lucide-react";
 
-const CreateOrEditNavMenu = withFallback(() => {
-  return (
-    <NestableItemsProvider>
-      <div className="flex flex-col md:flex-row max-w-screen-md gap-x-6">
-        <div className="basis-1/2">
-          <h2 className="mb-2">Add Menu Item</h2>
-          <AddItemForm />
+const CreateOrEditNavMenu = withFallback(
+  () => {
+    return (
+      <NestableItemsProvider>
+        <div className="flex flex-col md:flex-row max-w-screen-md gap-x-6">
+          <div className="basis-1/2">
+            <h2 className="mb-2">Add Menu Item</h2>
+            <AddItemForm />
+          </div>
+          <div className="basis-1/2">
+            <h2 className="mb-2">Menu Structure</h2>
+            <MenuEditor />
+          </div>
         </div>
-        <div className="basis-1/2">
-          <h2 className="mb-2">Menu Structure</h2>
-          <MenuEditor />
-        </div>
-      </div>
-    </NestableItemsProvider>
-  );
-});
+      </NestableItemsProvider>
+    );
+  },
+  () => <div>Loading Nav Menus...</div>,
+  ({ retry, error }) => (
+    <div>
+      <div>Failed to load Nav Menus: {error.message}</div>
+      <Button variant="destructive" onClick={() => retry()}>
+        <RotateCcw />
+        Retry
+      </Button>
+    </div>
+  ),
+);
 
 export default CreateOrEditNavMenu;
