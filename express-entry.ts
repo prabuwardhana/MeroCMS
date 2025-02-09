03801b01de4c30a98d82cd75e789a7089c53c97f
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-09 20:47:15 +0900

    add API route for managing navigation menu

--- express-entry.ts
+++ express-entry.ts
@@ -18,6 +18,7 @@ import userRoutes from "./server/routes/user.route";
 import postRoutes from "./server/routes/post.route";
 import mediaRoutes from "./server/routes/media.route";
 import categoryRoutes from "./server/routes/category.route";
+import navMenuRoutes from "./server/routes/navmenu.route";
 
 const isProduction = NODE_ENV === "production";
 
@@ -71,6 +72,7 @@ async function startServer() {
   app.use("/api/todo", todoRoutes);
   app.use("/api/post", postRoutes);
   app.use("/api/category", categoryRoutes);
+  app.use("/api/navmenu", navMenuRoutes);
   app.use("/api/media", mediaRoutes);
 
   /**
