cf55af3ea61c25a7730537dc740ccd283f1ecf77
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-10 19:34:54 +0900

    fix module not found error

--- pages/+Wrapper.tsx
+++ pages/+Wrapper.tsx
@@ -1,4 +1,4 @@
-import { CounterStoreProvider } from "@/providers/counterStoreProvider";
+import { CounterStoreProvider } from "@/components/Counter/providers/counterStoreProvider";
 import React from "react";
 import { usePageContext } from "vike-react/usePageContext";
 
