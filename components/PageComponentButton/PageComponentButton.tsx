d583191b36ff94805957804847c3c69d0477ac2c
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-14 21:01:10 +0900

    add PageComponentButton component

---
+++ components/PageComponentButton/PageComponentButton.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { GripVertical } from "lucide-react";
+import { useDraggable } from "@dnd-kit/core";
+import { PageComponentType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+
+const PageComponentButton = ({ pageComponent }: { pageComponent: PageComponentType }) => {
+  const draggable = useDraggable({
+    id: `component-btn-[${pageComponent.title}]`,
+    data: {
+      type: pageComponent.title,
+      isComponentBtn: true,
+    },
+  });
+  return (
+    <button
+      ref={draggable.setNodeRef}
+      className={cn(
+        "flex items-center gap-x-2 w-full h-9 border border-muted-foreground rounded-sm bg-gray-100 cursor-grab",
+        draggable.isDragging && "border-2 border-primary",
+      )}
+      {...draggable.listeners}
+      {...draggable.attributes}
+    >
+      <div className="flex items-center bg-gray-300 rounded-l-sm h-full px-1">
+        <GripVertical size={16} />
+      </div>
+      <div className="flex flex-row items-center py-2">{pageComponent.title}</div>
+    </button>
+  );
+};
+
+export const PageComponentButtonDragOverlay = ({ pageComponent }: { pageComponent: PageComponentType | undefined }) => {
+  return (
+    <button className="flex items-center gap-x-2 w-full h-9 border border-muted-foreground rounded-sm bg-gray-100 cursor-grab">
+      <div className="flex items-center bg-gray-300 rounded-l-sm h-full px-1">
+        <GripVertical size={16} />
+      </div>
+      <div className="flex flex-row items-center py-2">{pageComponent?.title}</div>
+    </button>
+  );
+};
+
+export default PageComponentButton;
