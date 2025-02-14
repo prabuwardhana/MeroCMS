e94531abaaa9957568fde35f094810a74a76c34f
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-14 20:55:54 +0900

    add RichText editor component

---
+++ components/RichTextEditor/ToolBar.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import type { Editor } from "@tiptap/react";
+import { Toggle } from "../ui/toggle";
+import {
+  Bold,
+  Italic,
+  Strikethrough,
+  AlignCenter,
+  AlignLeft,
+  AlignRight,
+  List,
+  ListOrdered,
+  Highlighter,
+} from "lucide-react";
+
+interface ToolBarProps {
+  editor: Editor | null;
+}
+
+export default function ToolBar({ editor }: ToolBarProps) {
+  if (!editor) return null;
+
+  const Options = [
+    {
+      icon: <Bold className="size-4" />,
+      onClick: () => editor.chain().focus().toggleBold().run(),
+      pressed: editor.isActive("bold"),
+    },
+    {
+      icon: <Italic className="size-4" />,
+      onClick: () => editor.chain().focus().toggleItalic().run(),
+      pressed: editor.isActive("italic"),
+    },
+    {
+      icon: <Strikethrough className="size-4" />,
+      onClick: () => editor.chain().focus().toggleStrike().run(),
+      pressed: editor.isActive("strike"),
+    },
+    {
+      icon: <AlignLeft className="size-4" />,
+      onClick: () => editor.chain().focus().setTextAlign("left").run(),
+      pressed: editor.isActive({ textAlign: "left" }),
+    },
+    {
+      icon: <AlignCenter className="size-4" />,
+      onClick: () => editor.chain().focus().setTextAlign("center").run(),
+      pressed: editor.isActive({ textAlign: "center" }),
+    },
+    {
+      icon: <AlignRight className="size-4" />,
+      onClick: () => editor.chain().focus().setTextAlign("right").run(),
+      pressed: editor.isActive({ textAlign: "right" }),
+    },
+    {
+      icon: <List className="size-4" />,
+      onClick: () => editor.chain().focus().toggleBulletList().run(),
+      pressed: editor.isActive("bulletList"),
+    },
+    {
+      icon: <ListOrdered className="size-4" />,
+      onClick: () => editor.chain().focus().toggleOrderedList().run(),
+      pressed: editor.isActive("orderedList"),
+    },
+    {
+      icon: <Highlighter className="size-4" />,
+      onClick: () => editor.chain().focus().toggleHighlight().run(),
+      pressed: editor.isActive("highlight"),
+    },
+  ];
+
+  return (
+    <div className="flex justify-start gap-1 flex-wrap border border-b-0 rounded-t-md border-primary/70 bg-background/20 p-1.5">
+      {Options.map((option, i) => (
+        <Toggle variant="default" key={i} size="xs" pressed={option.pressed} onPressedChange={option.onClick}>
+          {option.icon}
+        </Toggle>
+      ))}
+    </div>
+  );
+}
