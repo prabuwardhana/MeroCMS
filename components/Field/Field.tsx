35a2d6d769d444427de2cb3e0289a27239f0aeef
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-14 21:00:18 +0900

    add Field component

---
+++ components/Field/Field.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FieldValues, UseControllerProps } from "react-hook-form";
+import { LongText, RichText, ShortText } from "../TextInputs";
+
+interface FieldProps<T extends FieldValues> extends UseControllerProps<T> {
+  type: string;
+  label: string;
+  disabled?: boolean;
+}
+
+const Field = <T extends FieldValues>({ type, name, control, label }: FieldProps<T>) => {
+  return {
+    "rich-text": <RichText control={control} name={name} label={label} />,
+    "long-text": <LongText control={control} name={name} label={label} />,
+    "short-text": <ShortText name={name} label={label} />,
+  }[type];
+};
+
+export default Field;
