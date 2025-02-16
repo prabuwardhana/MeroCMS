fc87d9dab70df89a2184b1be789dc3fdfdfb89d1
prabuwardhana <prabuwardhana03@yahoo.com>
2025-02-17 00:45:23 +0900

    modify the markup for the single blog post page

--- pages/(site)/(blog)/@slug/+Page.tsx
+++ pages/(site)/(blog)/@slug/+Page.tsx
@@ -2,20 +2,42 @@ import React from "react";
 import { useData } from "vike-react/useData";
 import type { Data } from "./+data.js";
 import "@blocknote/shadcn/style.css";
+import { Badge } from "@/components/ui/badge.jsx";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.jsx";
 
 export default function Page() {
   const post = useData<Data>();
   return (
     <>
-      <h1>{post.title}</h1>
-      <img src={post.coverImageUrl} alt="" />
-      <h1>{post.author.name}</h1>
-      <ul>
-        {post.categories.map((category) => (
-          <li key={category}>{category}</li>
-        ))}
-      </ul>
-      <div className="" dangerouslySetInnerHTML={{ __html: post.htmlContent }}></div>
+      <div className="space-y-4">
+        <img
+          width={post.coverImage.width}
+          height={post.coverImage.height}
+          src={post.coverImage.url}
+          alt=""
+          className="h-80 w-full object-cover rounded-md"
+        />
+        <h1 className="text-5xl leading-snug font-bold text-center">{post.title}</h1>
+        <div className="w-full flex justify-center">
+          <ul className="m-0 p-0 flex">
+            {post.categories.map((category) => (
+              <li key={category}>
+                <Badge variant="outline">{category}</Badge>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex gap-4">
+          <Avatar>
+            <AvatarImage src={post.author.avatarUrl} className="object-cover" />
+            <AvatarFallback className="text-black">BT</AvatarFallback>
+          </Avatar>
+          <div className="flex items-center">
+            by&nbsp;<span className="font-bold">{post.author.name}</span>
+          </div>
+        </div>
+        <div className="" dangerouslySetInnerHTML={{ __html: post.htmlContent }}></div>
+      </div>
     </>
   );
 }
