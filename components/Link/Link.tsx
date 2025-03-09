b1f42391bbdc04bc1c725364bac2a364a438266d
prabuwardhana <prabuwardhana03@yahoo.com>
2025-03-09 21:21:14 +0900

    Link -> improve component logic and style

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { usePageContext } from "vike-react/usePageContext";
 
-export function Link({ href, children }: { href: string; children: React.ReactNode }) {
+export const Link = ({
+  href,
+  className,
+  children,
+}: {
+  href: string;
+  className?: string;
+  children: React.ReactNode;
+}) => {
   const pageContext = usePageContext();
   const { urlPathname } = pageContext;
-  const isActive = href === "/" ? urlPathname === href : urlPathname.startsWith(href);
+  const isActive = href ? (href === "/" ? urlPathname === href : urlPathname.startsWith(href)) : false;
   return (
-    <a href={href} className={isActive ? "is-active" : undefined}>
+    <a href={href} className={isActive && className ? className : undefined}>
       {children}
     </a>
   );
-}
+};
