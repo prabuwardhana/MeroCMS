2e68e43abe723fb96d78882d766180d9eb1677ff
prabuwardhana <prabuwardhana03@yahoo.com>
2025-03-10 13:25:03 +0900

    API client -> set different base URL for prod and dev

--- core/config/apiClient.ts
+++ core/config/apiClient.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const options = {
-  baseURL: `${import.meta.env.VITE_APP_BASE_URL}:${import.meta.env.VITE_PORT}`,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? `${import.meta.env.VITE_APP_BASE_URL}:${import.meta.env.VITE_PORT}`
+      : `${import.meta.env.VITE_APP_BASE_URL}`,
   withCredentials: true,
 };
 
